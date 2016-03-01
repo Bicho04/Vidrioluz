c57a65a56ef62f21f5abda1da1936c4f63623687
Bicho04 <Bicho04@gmail.com>
2016-02-29 23:52:48 -0300

    V0.2
    ya es responsive.

--- _script/functions.js
+++ _script/functions.js
@@ -1,42 +1,16 @@
-// var navigation = responsiveNav(".header-menu");
+var navigation = responsiveNav(".header-menu",{
+    label: "M",
+});
 
 $(function(){
-    // $('.casa-imagenes').owlCarousel({
-    //     autoplay: true,
-    //     autoplayTimeout: 20000,
-    //     autoplayHoverPause: true,
-    //     center: true,
-    //     loop: true,
-    //     margin: 0,
-    //     nav: false,
-    //     items: 1
-    // })
-      
-    // $('.casa-imagenes').owlCarousel({
-    //     autoplay: true,
-    //     autoplayTimeout: 5000,
-    //     autoplayHoverPause: true,
-    //     autoWidth: true,
-    //     center:  true,
-    //     loop: true,
-    //     margin: 50,
-    //     dots: true,
-    //         navText:  [ 'anterios', 'siguiente' ],
-    //     responsive: {
-    //         0: {
-    //             items: 2
-    //         },
-    //         600: {
-    //             items: 3
-    //         },
-    //         1000: {
-    //             items: 4
-    //         }
-    //     }
-    // })
-    $("#casa-imagenes").responsiveSlides({
-        manualControls: '#casa-imagenes-control',
-        speed: 600
+
+    $(".pictures").responsiveSlides({
+        speed: 600,
+        nav: true,
+        prevText: "<",
+        nextText: ">",   
+        navContainer: ".slide-navs",
+        namespace: "slide"
     });
 
 });
