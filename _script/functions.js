6c0fdf1f43220ffc25f50e2de9ad6de2c56e99dd
Bicho04 <Bicho04@gmail.com>
2016-03-04 18:14:54 -0300

    V0.2.9
    Correcciones en detalles de diseño

--- _script/functions.js
+++ _script/functions.js
@@ -1,14 +1,16 @@
 var navigation = responsiveNav(".header-menu",{
-    label: "M",
+    label: "",
 });
 
+
 $(function(){
+    $('.nav-toggle').addClass('fa fa-bars');
 
     $(".pictures").responsiveSlides({
         speed: 600,
         nav: true,
-        prevText: "<",
-        nextText: ">",   
+        prevText: "",
+        nextText: "",   
         navContainer: ".slide-navs",
         namespace: "slide"
     });
